608b22bd7ef69fbd9004396f31cd59d72223b90b
97gushan <97gustavh@gmail.com>
2015-09-09 15:39:41 +0200

    uppgift 4
    detta är uppgift 4 som jag har gjort, alla deluppgifter är i samma fil

---
+++ uppgifter/uppgift 4.py
@@ -0,0 +1,23 @@
+""" uppgift 4"""
+
+#a 
+for n in range(1,16):
+    print(n)
+    
+print("")
+
+#b    
+for n in range(20,31):
+    print(n)
+    
+print("")
+
+#c
+for n in range(0,1001):
+    print(n, end=" ")
+    
+print("")
+    
+#d
+for n in range(10,-11,-1):
+    print(n)
