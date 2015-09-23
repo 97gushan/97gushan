fe5942a3f15cefee41ff15bf2e0cfcf4a7711111
97gushan <97gustavh@gmail.com>
2015-09-23 15:46:18 +0200

    Added functions to Crop-class and an auto-grow function and a manual grow function
    Added a function to the crop class that updats the status of the crop
    
    Added a function to the crop class that makes the crop grow when called
    
    Added a auto-grow function that takes the crop-object and the amount of
    days that the crop are going to grow
    
    Added function so the user can manualy grow the crop for a day

--- cropfield grejen/Crop.py
+++ cropfield grejen/Crop.py
@@ -5,6 +5,9 @@ Created on Wed Sep 16 15:02:24 2015
 @author: 97gushan
 """
 
+import math
+from random import randint
+
 class Crop:
     """ This is a generic food crop """
     
@@ -29,9 +32,35 @@ class Crop:
         """ this method provides an overview of the Crop-class,
         it returns the values of type, status, growth, days_growing in a dictionary"""
         
-        return {'type': self._type, 'status': self._status,\
-                'growth': self._growth, 'days growing': self._days_growing}
-                
+        return ({'type': self._type, 'status': self._status,
+                'growth': self._growth, 'days growing': self._days_growing})
+    
+    
+    def _update_status(self):
+        """ this function updates the status of the crop"""
+
+        if(self._growth > 15):
+            self._status  = "old"
+        elif(self._growth > 10):
+            self._status = "mature"
+        elif(self._growth > 5):
+            self._status = "Seedling"
+        elif(self._growth == 0):
+            self._status = "seed"
+            
+    def grow(self, light, water):
+        """ This function grows the crop if the two arguments are more than
+            the light and water need"""        
+        
+        if(light >= self._light_need and water >= self._water_need):
+            self._growth += self.get_growth_rate()
+        
+        # increae amount of days growing
+        self._days_growing += 1
+
+        # update the status
+        self._update_status()        
+           
     # get methods
                 
                 
@@ -94,13 +123,66 @@ class Crop:
         """ this method takes a string as an argument and sets self._type to that value"""
         self._type = new_type
         
+
+def auto_grow(crop, days):
+    """ this function calls the grow method in the Crop-object and 
+        takes the object and amount of days as arguments"""
+    
+    for n in range(days):
+        # generate new amounts of water and light 
+        water = randint(1,10)
+        light = randint(1,10)
         
+        # call the grow-method 
+        crop.grow(light, water)
+    
+def manual_grow(crop):
+    """ this function lets the use chose the amount of water and 
+        light that the crop gets during one day"""
+    
+    valid = False
+    
+    # check if the light value inputed is a acceptable value
+    while(not valid):
+        try:
+            light = int(input("Please enter a light value (1-10): "))
+            
+            if( 1 <= light <= 10):
+                valid = True
+            else:
+                print("Value enterterd not valid- please enter a value between 1 and 10")
+        except ValueError:
+            print("Value enterter not valid - please enter a value between 1 and 10")
+            
+    valid = False
+    
+    #check if the water value inputed is a acceptable value
+    while(not valid):
+        try:
+            water = int(input("Please enter a water value (1-10): "))
+
+            if( 1 <= water <= 10):
+                valid = True
+            else:
+                print("Value enterter not valid - please enter a value between 1 and 10")
+        except ValueError:
+            print("Value enterter not valid - please enter a value between 1 and 10")
+
+    crop.grow(light, water)
+    
+
+
 def main():
     # instantiate the Crop class 
     crop = Crop(1,2,3)
     
     #print out some stuff
-    print(crop.needs())
+    print(crop.report())
+    
+    manual_grow(crop)    
+    
+    print(crop.report())
     
 if(__name__ == "__main__"):
     main()
+    
