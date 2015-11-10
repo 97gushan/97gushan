24af01a16998809eaee1ba19edd5aa9ca3a35938
97gushan <97gustavh@gmail.com>
2015-11-10 13:28:57 +0100

    removed previous method and created add_car
    Made a method that takes the maker, model price, year and amount of
    doors. it then calls the test_object.check_item_values() method to check
    if these values are the correct types. if yes, add a car to the vehicle
    list

--- exam1/model.py
+++ exam1/model.py
@@ -1,11 +1,17 @@
+import test
+import item
+
 
 class Model: 
     
     def __init__(self):
         
-        self.vehicle_list = [] 
+        self._vehicle_list = [] 
         
-        self.add_vehicle_through_command()
+        self.test_object = test
+        
+        #self.add_vehicle_through_command()
+        self.add_car("audi", "a8", "1000", "1990","2")
         
     def display_menu(self):
         """ this method displays the different vehicles 
@@ -89,5 +95,16 @@ class Model:
                         print("Please enter a year above 1 and below 10")
                 except ValueError:
                     print("Please enter a year above 1 and below 10")  
+        
+        
+        
+        
+    def add_car(self, maker, model,price, year, door_amount):
+        """ this method adds a car to the vehicle_list if all the values
+        are acceptable"""
+        
+        if(self.test_object.check_item_values(maker,model,price,year)):
+            self._vehicle_list.append(item.Car(door_amount,maker, model,price, year))
+        print(self._vehicle_list[0])
         
         
