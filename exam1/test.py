412a195e9074f52e9ef24fed63afbc93c536be55
97gushan <97gustavh@gmail.com>
2015-11-11 15:01:12 +0100

    added a method to test so the door_amount is an int
    I've added a test method that checks so the amount of doors is an int
    with a value above 0 and less or ecuall to 10

--- exam1/test.py
+++ exam1/test.py
@@ -15,4 +15,18 @@ def check_item_values(maker, model,price, year):
         is_valid = False
     
     return is_valid
+    
+def check_car_values(door_amount):
+    """ this method takes the door_amount and checks if they
+    are an int or not, if yes return true, else, return false"""
+    
+    
+    if(not door_amount.isdigit()):
+        return False
+    else:
+        # check so the door_amount is above 0 and below 10
+        if(door_amount > 0 and door_amount <= 10):
+            return True
+        else:
+            return False
 
