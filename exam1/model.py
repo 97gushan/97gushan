76886b954d29803d9aaf7a543b072f770510221b
97gushan <97gustavh@gmail.com>
2015-11-11 16:06:05 +0100

    created get_vehicle_list method
    This method returns the vehicle_list
    
    Also added some comments

--- exam1/model.py
+++ exam1/model.py
@@ -22,6 +22,8 @@ class Model:
         
         if(self.test_object.check_item_values(maker,model,price,year) and 
         self.test_object.check_car_values(door_amount)):
+            
+            # add car to vehicle_list
             self._vehicle_list.append(item.Car(door_amount,maker, model,price, year))
         else:
             print("values entered are not accepted")
@@ -33,6 +35,8 @@ class Model:
         
         if(self.test_object.check_item_values(maker,model,price,year) and 
         self.test_object.check_sm_values(seat_amount, reverse)):
+            
+            # add snowmobile to vehicle_list
             self._vehicle_list.append(item.Snowmobile(seat_amount, reverse,maker, model,price, year))
         else:
             print("values entered are not accepted")
@@ -44,5 +48,11 @@ class Model:
         """ this method takes a int as an argument and
         removes the object from vehicle_list with that index"""
         self._vehicle_list.pop(index)
+        
+        
+        
+    def get_vehicle_list(self):
+        """ this method returns the vehicle list"""
+        return self._vehicle_list
 
         
