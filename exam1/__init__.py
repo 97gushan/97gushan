f077bd6e35426daa6aa7b7b75f5445aaa6d8fa30
97gushan <97gustavh@gmail.com>
2015-11-17 13:34:47 +0100

    started the implementation of the gui
    added a main frame, a main layout and a first button. The button is
    connected to a method that for the moment only prints out a short string

--- exam1/__init__.py
+++ exam1/__init__.py
@@ -11,15 +11,29 @@ class Exam1(QMainWindow):
         
         self.setWindowTitle("Exam1 template")
         
-        self.initUI()
         
         self.model = model.Model()
+    
+        self.initUI()
+
     
     def initUI(self):
         """This method is called when the application first runs
             The method initializes all the gui """
-        pass
         
+        self.frame = QMainWindow(self)
+        self.setCentralWidget(self.frame)
+        
+        self.main_layout = QHBoxLayout()
+        
+        self.btn_add_vehicle = QPushButton("Add vehicle", self)
+        self.btn_add_vehicle.clicked.connect(self.open_add_vehicle_window)
+        
+        self.main_layout.addWidget(self.btn_add_vehicle)
+    
+    def open_add_vehicle_window(self):
+        print("open a new window")
+    
     def run(self):
         self.show()
         sys.exit(app.exec_())
