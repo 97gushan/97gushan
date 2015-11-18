03ee34790fcbc93e9f0155a4d717d18c77bbc26f
97gushan <97gustavh@gmail.com>
2015-11-18 19:15:52 +0100

    Added return statements to add_sm and add_car

--- exam1/model.py
+++ exam1/model.py
@@ -17,28 +17,39 @@ class Model:
         """ this method adds a car to the vehicle_list if all the values
         are acceptable"""
         
+        # call the test methods to see if the variables entered are acceptable
         if(self.test_object.check_item_values(maker,model,price,year) and 
         self.test_object.check_car_values(door_amount)):
             
             # add car to vehicle_list
             self._vehicle_list.append(item.Car(maker, model,price, year,door_amount))
+            
+            return True
         else:
             print("values entered are not accepted")
+            
+            return False
+        
         print(self._vehicle_list[0])
         
     def add_sm(self, maker, model, price, year, seat_amount, reverse):
         """ this method adds a snowmobile to the vehicle_list if all teh valeus
         are correct and acceptable"""
         
+        
+        # call the test methods to see if the variables entered are acceptable
         if(self.test_object.check_item_values(maker,model,price,year) and 
         self.test_object.check_sm_values(seat_amount, reverse)):
             
             # add snowmobile to vehicle_list
             self._vehicle_list.append(item.Snowmobile(maker, model,price, year,seat_amount, reverse))
+            
+            return True
         else:
             print("values entered are not accepted")
+            return False
         
-        #print(self._vehicle_list[0])
+        print(self._vehicle_list[0])
         
         
     def remove_vehicle(self, index):
